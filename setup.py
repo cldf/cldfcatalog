23fbfec7218c5e128356e81055af079273a05436
Robert Forkel <xrotwang@googlemail.com>
2021-11-25 08:51:12 +0100

    release 1.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cldfcatalog',
-    version='1.3.3.dev0',
+    version='1.4.0',
     author='Robert Forkel',
     author_email='dlce.rdm@eva.mpg.de',
     description='Python library to access CLDF reference catalogs',

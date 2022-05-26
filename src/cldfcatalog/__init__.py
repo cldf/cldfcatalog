3c5aaa7a9aae6f19c7d64ae1b32634b6bd7c12a2
Robert Forkel <xrotwang@googlemail.com>
2022-05-26 13:16:39 +0200

    bump version for development

--- src/cldfcatalog/__init__.py
+++ src/cldfcatalog/__init__.py
@@ -3,4 +3,4 @@ from cldfcatalog.catalog import *  # noqa: F401, F403
 from cldfcatalog.repository import *  # noqa: F401, F403
 from cldfcatalog.config import *  # noqa: F401, F403
 
-__version__ = '1.5.0'
+__version__ = '1.5.1.dev0'
